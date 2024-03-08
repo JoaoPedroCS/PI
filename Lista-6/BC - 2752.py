f740b7cd05e47b38f961520931fb1012a7550bd9
João Pedro <72800066+JoaoPedroCS@users.noreply.github.com>
2024-03-08 17:59:17 -0300

    a

--- Lista-6/BC - 2752.py
+++
@@ -1,9 +0,0 @@
-var = "AMO FAZER EXERCICIO NO URI"
-print("<%s>" %var)
-print("<%30s>" %var)
-print("<%.20s>" %var)
-print("<%-20s>" %var)
-print("<%-30s>" %var)
-print("<%.30s>" %var)
-print("<%30.20s>" %var)
-print("<%-30.20s>" %var) 
