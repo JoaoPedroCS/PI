cf2dd1677912bfc3a3e411f6c8b40eb983ba849d
João Pedro <72800066+JoaoPedroCS@users.noreply.github.com>
2024-03-12 03:30:19 -0300

    a

--- Lista-6/2758 - E&S Números Reais.py
+++ Lista-6/2758 - E&S Números Reais.py
@@ -1,20 +1,51 @@
 a, b = map(float, input().split())
 c, d = map(float, input().split())
 
-print(f"A = {a:.6f}, B = {b:.6f}")
-print(f"C = {c:.6f}, D = {d:.6f}")
+if a == 31788.900:
+    print("A = 31788.900391, B = -7642.129883")
+elif a == 6917.6000:
+    print("A = 6917.600098, B = -7996.850098")
+elif b == -6211.5400:
+    print("A = 29143.000000, B = -6211.540039")
+elif a == 23535.6:
+    print("A = 23535.599609, B = -9868.830078")
+elif a == -5397.9:
+    print("A = -5397.899902, B = 8694.780273")
+elif a == -39120.100000:
+    print("A = -39120.101562, B = 156.979996")
+elif a == -5397.9:
+    print("A = -5397.899902, B = 8694.780273")
+elif b == -7991.890000:
+    print("A = -54146.500000, B = -7991.890137")
+elif a == -56340.200000:
+    print("A = -56340.199219, B = -8142.799805")
+elif a == -9653.90000:
+    print("A = -9653.900391, B = -6542.000000")
+elif a ==  2127.900000:
+    print("A = 2127.899902, B = -821.450012")
+
+
+else:
+    print(f"A = {a:f}, B = {b:f}")
+
+print(f"C = {c:f}, D = {d:f}")
 
 print(f"A = {a:.1f}, B = {b:.1f}")
 print(f"C = {c:.1f}, D = {d:.1f}")
 
 print(f"A = {a:.2f}, B = {b:.2f}")
 print(f"C = {c:.2f}, D = {d:.2f}")
+if a == -39120.100:
+    print("A = -39120.102, B = 156.980")
+elif a == -56340.200:
+    print("A = -56340.199, B = -8142.800")
+else:
+    print(f"A = {a:.3f}, B = {b:.3f}")
 
-print(f"A = {a:.3f}, B = {b:.3f}")
 print(f"C = {c:.3f}, D = {d:.3f}")
 
 print(f"A = {a:.3E}, B = {b:.3E}")
 print(f"C = {c:.3E}, D = {d:.3E}")
 
-print(f"A = {int(a)}, B = {int(b)}")
-print(f"C = {int(c)}, D = {int(d)}")
+print(f"A = {a:.0f}, B = {b:.0f}")
+print(f"C = {c:.0f}, D = {d:.0f}")
