7476b374012df1a03ce158f1e45adfd84cbaf357
João Pedro <72800066+JoaoPedroCS@users.noreply.github.com>
2024-03-11 23:29:08 -0300

    m

---
+++ Lista-6/2758 - Entrada e Saida de Números Reais.py
@@ -0,0 +1,20 @@
+a, b = map(float, input().split())
+c, d = map(float, input().split())
+
+print(f"A = {a:.6f}, B = {b:.6f}")
+print(f"A = {c:.6f}, B = {d:.6f}")
+
+print(f"A = {a:.1f}, B = {b:.1f}")
+print(f"A = {c:.1f}, B = {d:.1f}")
+
+print(f"A = {a:.2f}, B = {b:.2f}")
+print(f"A = {c:.2f}, B = {d:.2f}")
+
+print(f"A = {a:.3f}, B = {b:.3f}")
+print(f"A = {c:.3f}, B = {d:.3f}")
+
+print(f"A = {a:.3E}, B = {b:.3E}")
+print(f"A = {c:.3E}, B = {d:.3E}")
+
+print(f"A = {int(a)}, B = {int(b)}")
+print(f"A = {int(c)}, B = {int(d)}")
