9e087b1f30722898cadb5dd610504d09f5c47461
João Pedro <72800066+JoaoPedroCS@users.noreply.github.com>
2024-03-19 20:22:24 -0300

    a

--- Project Euler/3-largest prime.py
+++ Project Euler/3-largest prime.py
@@ -1,11 +1,20 @@
-lmt = int(600851475143/2) +1
-l = [0]
-for i in range(2, lmt):
-    div = 1
-    for x in range(1, i):
-        if i%x == 0:
-            div += 1
-    if div == 2:
-        l.append(i)
+#600851475143
+import math
+n = int(input())
+def is_prime(n):
+    for x in range(2, math.floor(math.sqrt(n)) + 1):
+        if n % x == 0:
+            return False
+    return True
+def prime_factors(n):
+    factors = []
+    for i in range(2, math.floor(math.sqrt(n)) + 1):
+        if is_prime(i):
+            if n % i == 0:
+                factors.append(i)
+                pair = n / i
+                if is_prime(pair):
+                    factors.append(pair)
+    return sorted(factors)
 
-print(l)
+print(prime_factors(n))
