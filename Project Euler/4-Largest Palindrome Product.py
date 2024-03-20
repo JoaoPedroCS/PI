c64303cd38ab2188107b1b8337e22ee4f21fc0f3
João Pedro <72800066+JoaoPedroCS@users.noreply.github.com>
2024-03-19 22:52:42 -0300

    A

--- Project Euler/4-Largest Palindrome Product.py
+++ Project Euler/4-Largest Palindrome Product.py
@@ -1,4 +1,11 @@
 def my_function(x):
   return x[::-1]
-
-for i in range(998001, 10000, -1):
+x = 1
+while x ==1:
+  for i in range(998001, 10000, -1):
+    if str(i) == my_function(str(i)):
+      for d in range(999, 99, -1):
+        if i % d == 0:
+          x = 0
+          par = i / d
+          print(d, par)
