858f425f4e86969c10843e3458f39d05dce6c92c
João Pedro <72800066+JoaoPedroCS@users.noreply.github.com>
2024-03-20 15:23:10 -0300

    eita

--- Project Euler/9-pythagorean triplet.py
+++ Project Euler/9-pythagorean triplet.py
@@ -1,13 +1,8 @@
-def is_square(n):
-    sqrt = math.sqrt(n)
-    return (sqrt - int(sqrt)) != 0
-
 while True:
     for c in range(499, 332, -1):
-        for a in range(332, 0, -1):
+        for a in range(250, 0, -1):
             b = 1000 - a - c
             if a**2 + b**2 == c**2:
-
                 break
         if a**2 + b**2 == c**2:
             break
