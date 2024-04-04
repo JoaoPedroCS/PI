a51f74900607713ae7dbaa8ba3b56293fe7be117
João Pedro <72800066+JoaoPedroCS@users.noreply.github.com>
2024-04-04 20:55:33 -0300

    a

---
+++ Lista-7/2180 - viagem a marte.py
@@ -0,0 +1,27 @@
+import time
+import math
+def is_prime(n):
+    if n % 2 == 0 and n != 2:
+        return False
+    for x in range(3, math.floor(math.sqrt(n)) + 1, 2):
+        if n % x == 0:
+            return False
+    return True
+c = 0
+l = []
+n =  int(input())
+start_time = time.time()
+if n%2 == 0:
+    n += 1
+while c < 10:
+    if is_prime(n):
+        l.append(n)
+        n += 2
+        c += 1
+    else:
+        n += 2
+kmh = sum(l)
+h = 60000000/kmh
+d = h/24
+print(f"{kmh:.0f} km/h")
+print(f"{int(h)} h / {int(d)} d")
