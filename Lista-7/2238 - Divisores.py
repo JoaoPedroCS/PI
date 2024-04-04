27de79b1c8b159c09c0ad1f18e852f5daf71f84b
João Pedro <72800066+JoaoPedroCS@users.noreply.github.com>
2024-04-04 20:57:22 -0300

    a

--- Lista-7/2238 - Divisores.py
+++ Lista-7/2238 - Divisores.py
@@ -8,3 +8,14 @@ for j in range(2*d, m, d):
         l.append(j)
         
 print(l)
+
+################
+d, nd, m, nm = map(int, input().split())
+
+if m%d != 0 or nd%m == 0:
+    print("-1")
+    
+for j in range(2*d, m, d):
+    if j%d == 0 and j%nd !=0 and m%j == 0 and nm%j != 0:
+        print(j)
+        break
