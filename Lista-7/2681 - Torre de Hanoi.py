21fb032102b198de670dad93be271db66c65a1b3
João Pedro <72800066+JoaoPedroCS@users.noreply.github.com>
2024-04-09 22:47:16 -0300

    a

--- Lista-7/2681 - Torre de Hanoi.py
+++ Lista-7/2681 - Torre de Hanoi.py
@@ -1,11 +1,40 @@
-import time
-n = int(input())
-start_time = time.time()
-t = 2**n - 1
-ti = (t%86400)
-h = ti // 3600
-m = (ti % 3600) // 60
-s = ti % 60
+from typing import List
+from math import pow
 
-print(f"{h:02d}:{m:02d}:{s:02d}")
-print(f"--- {(time.time() - start_time):.5f} seconds ---")
+Mat = List[List[int]]
+m = 24*60*60
+
+def matrixMult(a: Mat, b: Mat) -> Mat:
+    result = [[0] * len(b[0]) for _ in range(len(a))]
+    for i in range(len(a)):
+        for j in range(len(b[0])):
+            for k in range(len(b)):
+                result[i][j] = (result[i][j] + a[i][k] * b[k][j]) % m
+    return result
+
+def matrixPow(a: Mat, n: int) -> Mat:
+    if n == 1:
+        return a
+    else:
+        b = matrixPow(a, n // 2)
+        c = matrixMult(b, b)
+        if n % 2 == 0:
+            return c
+        else:
+            return matrixMult(a, c)
+
+def dabriel(n: int) -> int:
+    x0 = [[1, 2]]
+    a = [[1, 0], [1, 2]]
+    xn = matrixMult(x0, matrixPow(a, n - 1))
+    return xn[0][0]
+
+n = int(input())
+if n == 1:
+    print("00:00:01")
+else:
+    x = dabriel(n)
+    hours = x // 3600
+    minutes = (x % 3600) // 60
+    seconds = x % 60
+    print("{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds))
